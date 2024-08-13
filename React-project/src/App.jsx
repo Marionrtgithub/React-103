a445539b0df98c73b46c817dd095be4a9292dfac
Kiprotich marion <marionjemutai71@gmail.com>
2024-08-13 23:14:08 +0300

    Create App.jsx

---
+++ React-project/src/App.jsx
@@ -0,0 +1,10 @@
+/*import MyCounter from "./MyCounter";
+function App(){
+  return(<MyCounter/>)
+}
+export default App;*/
+import WindowScreen from "./WindowScreen";
+function App(){
+  return(<WindowScreen/>)
+}
+export default App;
